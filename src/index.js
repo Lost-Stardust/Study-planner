87d10e72f3110d0637ddcf2ac1461cdc1b808781
Isa <isaismail3000@gmail.com>
2026-03-18 20:16:37 +0300

    Import ui.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import "../src/styles.css";
 
-// Style logic for current tab in the sidebar
-const options = document.querySelectorAll(".options div");
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    options.forEach((option) => {
-      option.classList.remove("active");
-    });
-    option.classList.add("active");
-  });
-});
+// js module imports
+import "./ui.js";
