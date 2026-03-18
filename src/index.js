72e0a4b46d77bc08de9d4b892219b1b317765e12
Isa <isaismail3000@gmail.com>
2026-03-18 20:08:07 +0300

    Add logic for current sidebar tab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "../src/styles.css";
+
+// Style logic for current tab in the sidebar
+const options = document.querySelectorAll(".options div");
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    options.forEach((option) => {
+      option.classList.remove("active");
+    });
+    option.classList.add("active");
+  });
+});
