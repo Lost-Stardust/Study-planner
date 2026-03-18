cb5d5043243cac0f4d9950f4c93b763bdbe800bf
Isa <isaismail3000@gmail.com>
2026-03-18 20:16:20 +0300

    Create module for ui logic, move previous here

---
+++ src/ui.js
@@ -0,0 +1,13 @@
+// Style logic for current tab in the sidebar
+const options = document.querySelectorAll(".options div");
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    options.forEach((option) => {
+      option.classList.remove("active");
+    });
+    option.classList.add("active");
+  });
+});
+
+export {};
