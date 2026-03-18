a3045608e6f93b1d1dd16aa1e1c1375344d39748
Isa <isaismail3000@gmail.com>
2026-03-18 21:19:14 +0300

    Add view changing logic & comments

--- src/ui.js
+++ src/ui.js
@@ -1,12 +1,25 @@
-// Style logic for current tab in the sidebar
+// Style logic for current tab in the sidebar and view switching
 const options = document.querySelectorAll(".options div");
+const views = [...document.querySelectorAll("#Dashboard, #Projects")];
 
 options.forEach((option) => {
   option.addEventListener("click", () => {
+    // remove active class from all options and views
     options.forEach((option) => {
       option.classList.remove("active");
     });
+    views.forEach((view) => {
+      view.classList.remove("active");
+    });
+    // add active class to the clicked option and corresponding view
     option.classList.add("active");
+    // get the corresponding view by matching the option text content with the view id
+    const view = document.querySelector(
+      `#${option.textContent.replace(/\s/g, "")}`,
+    );
+    console.log(view);
+
+    view.classList.add("active");
   });
 });
 
