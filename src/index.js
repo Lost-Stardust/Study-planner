ea9b8b904272b78c000a0048c58d4ca92460328e
Isa <isaismail3000@gmail.com>
2026-03-20 18:18:26 +0300

    Import project modules, add local storage

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import "../src/styles.css";
 
 // js module imports
 import "./ui.js";
+import { projectPopup } from "./project-popup";
+import { makeProject } from "./project-ui";
+import { projectList } from "./add-project";
+
+projectPopup();
+
+// project local storage
+const storedProjs = JSON.parse(localStorage.getItem("projList"));
+if (storedProjs.length !== 0) {
+  storedProjs.forEach((proj) => {
+    projectList.push(proj);
+    console.log(projectList);
+    makeProject(proj);
+  });
+}
