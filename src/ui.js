2248103921f1d4d36814682d9069c42c66ec13d6
Isa <isaismail3000@gmail.com>
2026-03-23 21:39:26 +0300

    Add logic for switching between pages

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,61 @@
+// logic for switching pages between sign up and main site
+const signUp = document.querySelector("#sign-up-page");
+const logIn = document.querySelector("#log-in-page");
+const forgotPass = document.querySelector("#forgot-pass-page");
+const wrapper = document.querySelector("#wrapper");
+
+const goToLogIn = document.querySelector("#go-to-log-in");
+const goToLogIn2 = document.querySelector("#go-to-log-in2");
+const goToSignUp = document.querySelector("#go-to-sign-up");
+const goToForgotPass = document.querySelector("#go-to-forgot-pass");
+
+const createAccBtn = document.querySelector(".create-acc");
+const logInBtn = document.querySelector(".log-in");
+const resetLink = document.querySelector(".reset-link");
+
+// Show sign up page first
+signUp.classList.add("active");
+
+// switch between sign up pages
+goToLogIn.addEventListener("click", () => {
+  signUp.classList.remove("active");
+  logIn.classList.add("active");
+});
+goToLogIn2.addEventListener("click", () => {
+  forgotPass.classList.remove("active");
+  logIn.classList.add("active");
+});
+
+goToSignUp.addEventListener("click", () => {
+  logIn.classList.remove("active");
+  signUp.classList.add("active");
+});
+goToForgotPass.addEventListener("click", () => {
+  logIn.classList.remove("active");
+  forgotPass.classList.add("active");
+});
+
+// go to main site
+createAccBtn.addEventListener("click", () => {
+  console.log("going to main page");
+
+  signUp.classList.remove("active");
+  wrapper.classList.add("active");
+});
+logInBtn.addEventListener("click", () => {
+  event.preventDefault();
+  console.log("going to main page");
+
+  logIn.classList.remove("active");
+  wrapper.classList.add("active");
+});
+resetLink.addEventListener("click", () => {
+  console.log("going to main page");
+
+  forgotPass.classList.remove("active");
+  wrapper.classList.add("active");
+});
+
 // Style logic for current tab in the sidebar and view switching
 const options = document.querySelectorAll(".options div");
 const views = [
