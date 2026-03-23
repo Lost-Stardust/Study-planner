da8d16116c7b8bfce58596e781f6f7989f94cd33
Isa <isaismail3000@gmail.com>
2026-03-23 15:31:17 +0300

    add todo edit function
    - Update selected todo with new title, description, date, priority, project
    - Move todo to different project container if changed
    - Refresh todo display elements with updated values
    - Save changes to localStorage todo list

---
+++ src/edit-todo.js
@@ -0,0 +1,51 @@
+// code adapted from https://github.com/Lost-Stardust/Todo-list
+
+import { todoList } from "./add-todo";
+import { index } from "./todo-ui";
+const editTodo = () => {
+  console.log(todoList[index]);
+  const title = document.querySelector("#edit-title").value;
+  const desc = document.querySelector("#edit-desc").value;
+  const date = document.querySelector("#edit-date").value;
+  const priority = document.querySelector(".edit-priority").value;
+  const project = document.querySelector("#edit-projectSelect").value;
+  // console.log({ title, desc, date, priority, project });
+
+  const selectedTodo = todoList[index];
+  selectedTodo.title = title;
+  selectedTodo.desc = desc;
+  selectedTodo.date = date;
+  selectedTodo.priority = priority;
+  console.log(selectedTodo);
+
+  const todo = document.querySelector(`.todo[data-index="${index}"]`);
+  console.log(todo);
+
+  if (selectedTodo.project !== project) {
+    selectedTodo.project = project;
+    const projectDisplay = document.querySelector(
+      `.${project}`.replace(/\s/g, ""),
+    );
+    projectDisplay.appendChild(todo);
+  }
+
+  const editTitle = todo.querySelector(".todo-title");
+  const editDesc = todo.querySelector(".desc");
+  const editDate = todo.querySelector(".todo-date");
+  const editPriority = todo.querySelector(".todo-priority");
+  console.log(editTitle, editDesc, editDate, editPriority);
+
+  editTitle.textContent = title;
+  editDesc.textContent = desc;
+  editDate.textContent = date;
+  editPriority.textContent = priority;
+
+  const storedList = JSON.parse(localStorage.getItem("todoList"));
+  storedList[index].title = title;
+  storedList[index].desc = desc;
+  storedList[index].date = date;
+  storedList[index].priority = priority;
+  storedList[index].project = project;
+  localStorage.setItem("todoList", JSON.stringify(storedList));
+};
+export { editTodo };
